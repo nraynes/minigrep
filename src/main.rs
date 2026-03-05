08a8439b2e50ddb2d2ae6d19f7c4f7e6c9390c07
nicholasraynes <nicholasraynes@yahoo.com>
2026-03-05 16:23:09 -0500

    feat: added cli argument reading and display contents of file to console

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
+use std::env;
+use std::fs;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+    let query = &args[1];
+    let file_path = &args[2];
+
+    let contents = fs::read_to_string(file_path)
+        .expect("There was a problem reading the file!");
+
+    println!("The query is {query}!");
+    println!("The file path is {file_path}!");
+
+    println!("The file has the contents:\n{contents}");
 }
