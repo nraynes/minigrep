ea991f73b28824eba0d986f67fac07b114130847
nicholasraynes <nicholasraynes@yahoo.com>
2026-03-05 16:24:08 -0500

    refactor: moved config parsing to separate function

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,16 @@
 use std::env;
 use std::fs;
 
-fn main() {
-    let args: Vec<String> = env::args().collect();
+fn parse_config(args: &Vec<String>) -> (&str, &str) {
     let query = &args[1];
     let file_path = &args[2];
+
+    (query, file_path)
+}
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+    let (query, file_path) = parse_config(&args);
 
     let contents = fs::read_to_string(file_path)
         .expect("There was a problem reading the file!");
