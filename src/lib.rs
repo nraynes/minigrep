36cbd30bfb2f6ac44f9f16275d71df10e0c94f61
nicholasraynes <nicholasraynes@yahoo.com>
2026-03-05 18:17:51 -0500

    feat: added search functionality

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,13 @@
 pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
-    unimplemented!();
+    let mut result = Vec::new();
+
+    for line in contents.lines() {
+        if line.contains(query) {
+            result.push(line);
+        }
+    }
+
+    result
 }
 
 #[cfg(test)]
