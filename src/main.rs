e18a2b78250f815002b8a334b7c90ea779a1cbcd
nicholasraynes <nicholasraynes@yahoo.com>
2026-03-05 19:07:22 -0500

    refactor: made error output to stderr

--- src/main.rs
+++ src/main.rs
@@ -43,7 +43,7 @@ fn run(config: Config) -> Result<(), Box<dyn Error>> {
 fn main() {
     let args: Vec<String> = env::args().collect();
     let config: Config = Config::build(&args).unwrap_or_else(|err| {
-        println!("Problem parsing arguments: {err}");
+        eprintln!("Problem parsing arguments: {err}");
         process::exit(1);
     });
 
@@ -51,7 +51,7 @@ fn main() {
     println!("The file path is {}!", config.file_path);
 
     if let Err(e) = run(config) {
-        println!("Application error: {e}");
+        eprintln!("Application error: {e}");
         process::exit(1);
     }
 }
