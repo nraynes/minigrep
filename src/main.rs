e5362b59b60118587091e780447a9ba9bef6d3c0
nicholasraynes <nicholasraynes@yahoo.com>
2026-03-05 19:04:46 -0500

    feat: added case insensitive search to binary

--- src/main.rs
+++ src/main.rs
@@ -2,11 +2,12 @@ use std::env;
 use std::fs;
 use std::process;
 use std::error::Error;
-use minigrep::search;
+use minigrep::{ search, search_case_insensitive };
 
-struct Config {
-    query: String,
-    file_path: String,
+pub struct Config {
+    pub query: String,
+    pub file_path: String,
+    pub ignore_case: bool,
 }
 
 impl Config {
@@ -17,14 +18,22 @@ impl Config {
         let query: String = args[1].clone();
         let file_path: String = args[2].clone();
 
-        Ok(Config { query, file_path })
+        let ignore_case = env::var("IGNORE_CASE").is_ok();
+
+        Ok(Config { query, file_path, ignore_case })
     }
 }
 
 fn run(config: Config) -> Result<(), Box<dyn Error>> {
     let contents: String = fs::read_to_string(config.file_path)?;
 
-    for line in search(&config.query, &contents) {
+    let results = if config.ignore_case {
+        search_case_insensitive(&config.query, &contents)
+    } else {
+        search(&config.query, &contents)
+    };
+
+    for line in results {
         println!("{line}");
     }
 
